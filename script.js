9e1feb0dd5f917e5c8c1e244734cc4075a1267b3
edujuarez <eduardo._.juarez@live.com.ar>
2020-02-26 15:42:23 -0300

    primerCommit

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+    console.log('se ha cargado la pagina');
+    var searchBtn = document.getElementById('searchBtn');
+    console.log(searchBtn);
+});
